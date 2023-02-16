979d09e03cd1fd345977b5c6775c1fbe88abf9a1
jnnieradko <jn.nieradko@gmail.com>
2023-02-16 14:40:36 +0100

    view - Pokemondetails WIP

--- Pokemon/src/Main.elm
+++ Pokemon/src/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (..)
 
 import Browser
+import Cmd.Extra exposing (..)
 import Debug exposing (todo)
 import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (src)
-import Cmd.Extra exposing (..)
 import Html.Events exposing (onClick)
+import Http exposing (..)
+import Json.Decode as JD exposing (Decoder)
 
 
 
@@ -31,8 +33,8 @@ init =
 
 type Msg
     = GetPokemonList
-    | GotPokemonList (List PokemonName)
-    | GetPokemonDetails PokemonName
+    | GotPokemonList (Result Http.Error (List PokemonName))
+    | GetPokemonDetails PokemonNameUrl
     | GotPokemonDetails Pokemon
     | ErrMsg String
 
@@ -45,20 +47,38 @@ type alias PokemonName =
 
 type alias Pokemon =
     { abilities : List Ability
-     , back_default : String}
+    , back_default : String
+    }
 
+type alias PokemonNameUrl = String
 
-type alias Ability = String
+type alias Ability =
+    String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GetPokemonList -> (Loading , getPokemonList)
-        GotPokemonList lpn -> (PokemonList lpn , Cmd.none)
-        GetPokemonDetails pn -> (model, getpokemonDetails pn)
-        GotPokemonDetails p -> (PokemonDetails p , Cmd.none)
-        ErrMsg s -> (ErrorModel s , Cmd.none)
+        GetPokemonList ->
+            ( Loading, getPokemonList )
+
+        GotPokemonList relpn ->
+            case relpn of
+                Err e ->
+                    ( ErrorModel "Błąd", Cmd.none )
+
+                Ok v ->
+                    ( PokemonList v, Cmd.none )
+
+        GetPokemonDetails pnUrl ->
+            ( model, getpokemonDetails pnUrl )
+
+        GotPokemonDetails p ->
+            ( PokemonDetails p, Cmd.none )
+
+        ErrMsg s ->
+            ( ErrorModel s, Cmd.none )
+
 
 
 ---- VIEW ----
@@ -67,11 +87,20 @@ update msg model =
 view : Model -> Html Msg
 view model =
     case model of
-        LandingPage -> div [] [text "Hello !" , button [onClick GetPokemonList ] [text " Wyświetl listę Pokemonów"]]
-        Loading -> div [] [text "laduję dane ... "]
-        PokemonList lpn ->  div [] (listOfPokemonNames lpn)
-        PokemonDetails p -> div [] [text "dane szczegółowe o Pokemonie" , button [onClick GetPokemonList ] [text "Wróć do listy Pokemonów"]]
-        _ -> div [] []
+        LandingPage ->
+            div [] [ text "Hello !", button [ onClick GetPokemonList ] [ text " Wyświetl listę Pokemonów" ] ]
+
+        Loading ->
+            div [] [ text "laduję dane ... " ]
+
+        PokemonList lpn ->
+            div [] (listOfPokemonNames lpn )
+
+        PokemonDetails p ->
+            div [] [ text "dane szczegółowe o Pokemonie", button [ onClick GetPokemonList ] [ text "Wróć do listy Pokemonów" ]  ]
+
+        _ ->
+            div [] []
 
 
 
@@ -89,11 +118,48 @@ main =
 
 
 
-getPokemonList : Cmd Msg
-getPokemonList = Cmd.Extra.perform (GotPokemonList [{name = "Pikaczu" , url = "e-orzecznik.pl"}])
+-- Http request Get -- return list of Pokemon's names
 
-getpokemonDetails : PokemonName -> Cmd Msg
-getpokemonDetails pn = Cmd.Extra.perform (GotPokemonDetails {abilities = [] , back_default = pn.url})
+
+getPokemonList : Cmd Msg
+getPokemonList =
+    Http.get
+        { url = "https://pokeapi.co/api/v2/pokemon"
+        , expect = Http.expectJson GotPokemonList dekoder
+        }
+
+
+funkcja =
+    todo ""
+
+
+dekoderPok : Decoder PokemonName
+dekoderPok = JD.map2 PokemonName
+                (JD.field "name" JD.string)
+                (JD.field "url" JD.string)
+
+
+dekoder : Decoder (List PokemonName)
+dekoder = JD.at ["results"] (JD.list dekoderPok)
+
+
+
+
+
+
+--Cmd.Extra.perform (GotPokemonList [{name = "Pikaczu" , url = "e-orzecznik.pl"}])
+-- Http request Get -- return Pokemon's details
+
+
+getpokemonDetails : PokemonNameUrl -> Cmd Msg
+getpokemonDetails pnUrl =
+    Cmd.Extra.perform (GotPokemonDetails { abilities = [], back_default = pnUrl })
+
+
+
+-- function used in view to display list of pokemon's name
+
 
 listOfPokemonNames : List PokemonName -> List (Html Msg)
-listOfPokemonNames lpn = List.map(\pn -> div [onClick (GetPokemonDetails pn) ] [text pn.name])lpn
+listOfPokemonNames lpn =
+    List.map (\pn -> div [ onClick (GetPokemonDetails pn.url) ] [ text pn.name ]) lpn
